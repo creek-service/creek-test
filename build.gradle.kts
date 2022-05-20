6e086ad9b2a0e13b42a4d5588c40ccacb6e9518c
Big Andy <8012398+big-andy-coates@users.noreply.github.com>
2022-05-20 14:12:34 +0100

    Fix issue with conformity testing unnamed modules
    Conformity testing was failing if running with unnamed modules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,6 @@ project.version = scmVersion.version
 allprojects {
     apply(plugin = "idea")
     apply(plugin = "java")
-    apply(plugin = "jacoco")
     apply(plugin = "checkstyle")
     apply(plugin = "com.diffplug.spotless")
     apply(plugin = "com.github.spotbugs")
@@ -45,7 +44,11 @@ allprojects {
 
 subprojects {
     apply(plugin = "maven-publish")
-    apply(plugin = "org.javamodularity.moduleplugin")
+
+    if (!name.startsWith("test-")) {
+        apply(plugin = "org.javamodularity.moduleplugin")
+        apply(plugin = "jacoco")
+    }
 
     project.version = project.parent?.version!!
 
@@ -128,7 +131,7 @@ subprojects {
         }
     }
 
-    tasks.jacocoTestReport {
+    tasks.withType<JacocoReport>().configureEach{
         dependsOn(tasks.test)
     }
 
@@ -144,24 +147,26 @@ subprojects {
         dependsOn("checkstyleMain", "checkstyleTest", "spotbugsMain", "spotbugsTest")
     }
 
-    publishing {
-        repositories {
-            maven {
-                name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/creek-service/${rootProject.name}")
-                credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
+    if (!name.startsWith("test-")) {
+        publishing {
+            repositories {
+                maven {
+                    name = "GitHubPackages"
+                    url = uri("https://maven.pkg.github.com/creek-service/${rootProject.name}")
+                    credentials {
+                        username = System.getenv("GITHUB_ACTOR")
+                        password = System.getenv("GITHUB_TOKEN")
+                    }
                 }
             }
-        }
-        publications {
-            create<MavenPublication>("maven") {
-                from(components["java"])
-                artifactId = "${rootProject.name}-${project.name}"
+            publications {
+                create<MavenPublication>("maven") {
+                    from(components["java"])
+                    artifactId = "${rootProject.name}-${project.name}"
 
-                pom {
-                    url.set("https://github.com/creek-service/${rootProject.name}.git")
+                    pom {
+                        url.set("https://github.com/creek-service/${rootProject.name}.git")
+                    }
                 }
             }
         }
---
+++ test-unnamed/src/test/java/org/creekservice/ModuleTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2022 Creek Contributors (https://github.com/creek-service)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.creekservice;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.creekservice.api.test.conformity.ConformityTester;
+import org.creekservice.api.test.conformity.check.CheckConstructorsPrivate;
+import org.creekservice.api.test.conformity.check.CheckModule;
+import org.creekservice.api.unnamed.ApiTypeWithPublicConstructor;
+import org.junit.jupiter.api.Test;
+
+class ModuleTest {
+
+    @Test
+    void shouldBeUnnamed() {
+        assertThat(ModuleTest.class.getModule().isNamed(), is(false));
+    }
+
+    @Test
+    void shouldFailOnBadTypes() {
+        // Given:
+        final ConformityTester tester =
+                ConformityTester.builder(ModuleTest.class)
+                        .withDisabled("not a module", CheckModule.builder());
+
+        // When:
+        final Error e = assertThrows(AssertionError.class, tester::check);
+
+        // Then:
+        assertThat(e.getMessage(), containsString(ApiTypeWithPublicConstructor.class.getName()));
+    }
+
+    @Test
+    void shouldPassConformityFromUnnamedModule() {
+        ConformityTester.builder(ModuleTest.class)
+                .withDisabled("not a module", CheckModule.builder())
+                .withCustom(
+                        "deliberately bad type",
+                        CheckConstructorsPrivate.builder()
+                                .excludedClasses(ApiTypeWithPublicConstructor.class))
+                .check();
+    }
+}
